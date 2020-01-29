78bd3e7b2e58abae4c39d65dc9ad0af2b05fb62a
REDDYCHETANA123 <60418692+REDDYCHETANA123@users.noreply.github.com>
2020-01-29 10:56:36 +0530

    Create p.py

---
+++ p.py
@@ -0,0 +1,16 @@
+def add(a,b):
+    sum=a+b
+    print(sum)
+add(10,2)
+def sub(a,b):
+    sub=a-b
+    print(sub)
+sub(10,2)
+def mul(a,b):
+    mul=a*b 
+    print(mul)
+mul(10,2)
+def div(a,b):
+    c=a/b
+   print(c)
+div(10,2)
